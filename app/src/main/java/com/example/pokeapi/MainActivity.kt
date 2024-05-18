d58827c015d39a7db0e1e50a3a7dfba4441ba700
Enriquermg21 <enriquermg21@gmail.com>
2024-05-19 01:18:23 +0200

    loading view and modifying the background

--- app/src/main/java/com/example/pokeapi/MainActivity.kt
+++ app/src/main/java/com/example/pokeapi/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.pokeapi
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokeapi.databinding.ActivityMainBinding
+import com.example.pokeapi.databinding.LoadingViewBinding
 import dataRetrofit.RetrofitService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,11 +25,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingLoading: LoadingViewBinding
     private lateinit var adapter: PokeAdapter
     private val pokeObj = mutableListOf<String>()
     private val pokeSprite = mutableListOf<String>()
     private var selectedPokemonName: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,7 +40,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         binding = ActivityMainBinding.inflate(layoutInflater)
+        bindingLoading = LoadingViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        showLoadingView()
 
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = PokeAdapter(pokeObj, pokeSprite) { capitalizedPokemonName ->
@@ -46,8 +51,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         binding.recyclerView.adapter = adapter
-        val intent = Intent(this, pokeinfoActivity::class.java).apply {}
-        startActivity(intent)
+
         getPokemonList()
     }
 
@@ -93,20 +97,25 @@ class MainActivity : AppCompatActivity() {
                     if (call.isSuccessful && pokeResponseSprite != null) {
                         val sprite = pokeResponseSprite.sprites.frontDefault
                         sprites.add(sprite)
+
                     } else {
-                        sprites.add("") // Manejar la ausencia de sprite
+                        sprites.add("")
                     }
                 } catch (e: Exception) {
-                    sprites.add("") // Manejar la ausencia de sprite en caso de error
+                    sprites.add("")
                 }
             }
+
             withContext(Dispatchers.Main) {
                 pokeSprite.clear()
                 pokeSprite.addAll(sprites)
                 adapter.notifyDataSetChanged()
+                hideLoadingView()
+                Log.d("getPokemonSpritesFull", "Updated adapter with new sprites")
             }
         }
     }
+
     private fun hideKeyboard() {
         val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(binding.main.windowToken, 0)
@@ -114,5 +123,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun showError() {
         Toast.makeText(this, "Error machote", Toast.LENGTH_LONG).show()
+    }
+
+    private fun showLoadingView() {
+        binding.layoutloading.loadingView.visibility = View.VISIBLE
+    }
+
+    private fun hideLoadingView() {
+        binding.layoutloading.loadingView.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/pokeapi/PokeResponseSprite.kt
+++ app/src/main/java/com/example/pokeapi/PokeResponseSprite.kt
@@ -3,11 +3,15 @@ package com.example.pokeapi
 import com.google.gson.annotations.SerializedName
 
 data class PokeResponseSprite(
-    @SerializedName("id") val id: Int,
-    @SerializedName("name") val name: String,
+    // @SerializedName("types") val types: Types,
     @SerializedName("sprites") val sprites: Sprites
+
 )
 
 data class Sprites(
     @SerializedName("front_default") val frontDefault: String
+)
+
+data class Types(
+    @SerializedName("name") val name: String
 )
